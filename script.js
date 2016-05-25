cc82048270f1f94a4f938d7a91fd6465871713f4
HuangShan <Shan@HuangShans-iMac.fios-router.home>
2016-05-25 00:13:48 -0400

    Lesson 2

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("Hello Lei <3");
+$(document).ready(function () {
+	
+	$("#tom").click(function () {
+		$("#myDiv").text("Hi Tom and Jerry!");
+	});
+	$("#jerry").click(function () {
+		$("#myDiv").text("Please loosen my buttons");
+	});
+});
+
